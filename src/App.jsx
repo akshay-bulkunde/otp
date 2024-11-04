13db21f193e2a45023422dc1cf23bd8625895ace
akshay-bulkunde <akshay.bulkunde.29.3@mountblue.tech>
2024-11-04 21:01:00 +0530

    Implement otp using useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,36 @@
-import React, { useState } from "react";
+import React, { useRef } from "react";
 
 const App = () => {
-  const [otp, setOtp] = useState(["", "", "", ""]);
+  const ref = useRef([]);
 
   const handleChange = (e, idx) => {
-    const value = e.target.value;
-    const newOtp = [...otp];
-    newOtp[idx] = value;
-    setOtp(newOtp);
-
-    
-    if (value.length === 1 && idx < otp.length - 1) {
-      document.getElementById(`otp-input-${idx + 1}`).focus();
+    const { value } = e.target;
+    if (value.length === 1 && idx < ref.current.length - 1) {
+      ref.current[idx + 1].focus();
     }
-    
-    
   };
 
   const handleKeyDown = (e, idx) => {
-    if (e.key === "Backspace" && !otp[idx] && idx > 0) {
-      document.getElementById(`otp-input-${idx - 1}`).focus();
+    if (e.key === "Backspace" && !e.target.value && idx > 0) {
+      ref.current[idx - 1].focus();
     }
   };
 
   return (
     <div className="flex justify-center items-center gap-4 mt-7">
-      {otp.map((_, idx) => (
-        <input
-          key={idx}
-          id={`otp-input-${idx}`}
-          value={otp[idx]}
-          onChange={(e) => handleChange(e, idx)}
-          onKeyDown={(e) => handleKeyDown(e, idx)}
-          type="text"
-          maxLength={1}
-          className="p-2 w-8 border border-gray-500"
-        />
-      ))}
+      {Array(4)
+        .fill(null)
+        .map((_, idx) => (
+          <input
+            key={idx}
+            ref={(el) => (ref.current[idx] = el)}
+            onChange={(e) => handleChange(e, idx)}
+            onKeyDown={(e) => handleKeyDown(e, idx)}
+            type="text"
+            maxLength={1}
+            className="p-2 w-8 border border-gray-500 text-center"
+          />
+        ))}
     </div>
   );
 };
